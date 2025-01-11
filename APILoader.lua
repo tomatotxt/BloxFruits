c9cd43eeddba0c47c6c1e3d5be2e12e589cad409
ImMejor35 <cheesygarilla2@gmail.com>
2025-01-10 20:21:59 -0500

    fix: correct repository URL in APILoader and main scripts

--- APILoader.lua
+++ APILoader.lua
@@ -1,7 +1,7 @@
 getgenv().branch = "main"
 getgenv().owner = "ImMejor35"
 getgenv().git = function(path: string)
-    local result = "https://github.com/" .. owner .. "/Blox-Fruits/raw/refs/heads/" .. branch .. "/" .. path
+    local result = "https://github.com/" .. owner .. "/BloxFruits/raw/refs/heads/" .. branch .. "/" .. path
     return loadstring(game:HttpGet(result))()
 end
 
