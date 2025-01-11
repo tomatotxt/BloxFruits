45703039a9b299ea1348e77a7d24162b5158083e
ImMejor35 <cheesygarilla2@gmail.com>
2025-01-10 22:37:15 -0500

    feat: enhance API loading mechanism with version control and folder initialization

--- API/APILoader.lua
+++ API/APILoader.lua
@@ -1,13 +1,44 @@
 getgenv().branch = "main"
 getgenv().owner = "ImMejor35"
-local git = function(path: string)
-    local result = "https://github.com/" .. owner .. "/BloxFruits/raw/refs/heads/" .. branch .. "/" .. path
-    return loadstring(game:HttpGet(result))()
+local API_VERSION = 1.0
+
+-- Intialize BloxFruits Folder if missing..
+if not isfolder("BloxFruits") then makefolder("BloxFruits") end
+
+local git = function(path)
+    -- Enter BloxFruits Folder
+    local realpath = "BloxFruits/" .. path
+
+    -- Create API Folder if missing..
+    if not isfolder("BloxFruits/API") then makefolder("BloxFruits/API") end
+
+    -- Check if API is up-to-date.
+    local Change = false
+    if not isfile("BloxFruits/API/version.txt") then
+        writefile("BloxFruits/API/version.txt", tostring(API_VERSION))
+    elseif readfile("BloxFruits/API/version.txt") ~= tostring(API_VERSION) then
+        writefile("BloxFruits/API/version.txt", tostring(API_VERSION))
+        Change = true
+    end
+
+    -- Write API to file if missing or if change is needed.
+    local result = nil;
+    if not isfile(realpath) or Change then
+        local url = string.format("https://github.com/%s/BloxFruits/raw/refs/heads/%s/%s", owner, branch, path)
+        result = game:HttpGet(url)
+        writefile(realpath, result)
+    end
+
+    -- Return API
+    if Change then
+        return loadstring(result)()
+    end
+    return loadstring(readfile(realpath))()
 end
 
-wait = task.wait
 getgenv().OptionsAPI = git("API/OptionsAPI.lua")
 getgenv().ServicesAPI = git("API/ServicesAPI.lua")
 getgenv().PlayerAPI = git("API/DynamicAPI.lua")
 getgenv().TeleportAPI = git("API/TeleportAPI.lua")
 getgenv().RemoteAPI = git("API/RemoteAPI.lua")
+print("API's Loaded")
